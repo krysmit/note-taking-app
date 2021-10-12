3e3596481a1c35dea162bd50fa91430c6493cfec
Krystyn Smith <krskrsmith@gmail.com>
2021-10-11 21:37:25 -0500

    routed notes.html

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const fs = require('fs');
 const PORT = process.env.port || 3001;
 const app = express();
 
-app.get("/", (req, res) => {
-    console.log('get working')
-    res.send("HELLO")
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//to get notes.html
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'),
+    console.log("notes connection achieved!")
 })
 app.listen(3000)
