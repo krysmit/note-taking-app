b5724d0f2647b366e2f2584720396e1d8bb0aa48
Krystyn Smith <krskrsmith@gmail.com>
2021-10-12 19:58:46 -0500

    working on errors in console

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,7 +2,7 @@ const app = require('express').Router();
 const fs = require('fs');
 const db = require('../db/db.json');
 app.get('/api/notes', function(req, res){
-    db = fs.readFileSync("./db/db.json")
+    fs.readFileSync("../db/db.json")
     console.log('get', db)
     res.json(db);
 });
