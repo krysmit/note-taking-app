52c83198734e94b86d8e91d0b53c394a01dde0d6
Krystyn Smith <krskrsmith@gmail.com>
2021-10-12 20:50:56 -0500

    figured out some of the errors, still working on some others that have come up

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,10 +1,11 @@
 const app = require('express').Router();
 const fs = require('fs');
-const db = require('./db/db.json');
+// const db = require('./db/db.json');
+
 app.get('/api/notes', function(req, res){
-    fs.readFileSync("./db/db.json")
-    console.log('get', db)
-    res.json(db);
+    fs.readFileSync('db/db.json', 'utf8');
+    // console.log('get', db)
+    // res.json(db);
 });
 
 module.exports = app;
